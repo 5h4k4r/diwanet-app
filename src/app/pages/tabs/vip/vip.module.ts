7d14e860efdbf49c177b90a07b3ea4c851819049
5h4k4r <shakar.halabjai@yahoo.com>
2021-11-24 14:10:15 +0300

    Updated vip page

--- src/app/pages/tabs/vip/vip.module.ts
+++ src/app/pages/tabs/vip/vip.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { VipPageRoutingModule } from './vip-routing.module';
 
 import { VipPage } from './vip.page';
+import { NgxStarRatingModule } from 'ngx-star-rating';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
-    VipPageRoutingModule
+    VipPageRoutingModule,
+    SharedModule
   ],
   declarations: [VipPage]
 })
-export class VipPageModule {}
+export class VipPageModule { }
--- src/app/pages/tabs/vip/vip.page.ts
+++ src/app/pages/tabs/vip/vip.page.ts
@@ -1,15 +1,98 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { merge, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { Category } from 'src/app/backend/models/category.model';
+import { ServiceMan } from 'src/app/backend/models/serviceman.model';
+import { CategoriesService } from 'src/app/backend/services/categories.service';
+import { UsersService } from 'src/app/backend/services/users.service';
+import { UserProfileComponent } from 'src/app/shared/components/user-profile/user-profile.component';
 
 @Component({
   selector: 'app-vip',
   templateUrl: './vip.page.html',
   styleUrls: ['./vip.page.scss'],
 })
-export class VipPage implements OnInit {
+export class VipPage implements OnInit, OnDestroy {
+  //#region Fields
+  searchText = '';
+  stars = 3;
+  _selectedCategory = 'all';
 
-  constructor() { }
+  private _loading = false;
+  private _categories: Category[] = [];
+  private _users: ServiceMan[] = [];
+  private _getDataEvent = new EventEmitter();
+  private _destroy$ = new Subject();
+  //#endregion
 
-  ngOnInit() {
+
+  //#region Properties
+  get loading(): boolean { return this._loading; }
+  get users(): any { return this._users; }
+  get categories(): Category[] { return this._categories; }
+  //#endregion
+
+
+  //#region Constructor
+
+
+  constructor(
+    private usersService: UsersService,
+    private categoriesService: CategoriesService,
+  ) { }
+
+
+  //#endregion
+
+
+
+  //#region Functions
+
+
+  async ngOnInit(): Promise<void> {
+    this._loading = true;
+
+    this._categories = await this.categoriesService.listCategories();
+
+    this._loading = false;
+
+    this.loadDataAsync();
+    merge(this._getDataEvent).pipe(takeUntil(this._destroy$)).subscribe(_ => {
+      this.loadDataAsync();
+    });
   }
+
+  onSearchChange(event: any): void {
+    const value = event.target.value;
+    if (value === this.searchText) return;
+
+    this.searchText = value;
+    this._getDataEvent.emit();
+  }
+  async loadDataAsync(): Promise<void> {
+    this._loading = true;
+
+    try {
+      this._users = await this.usersService.listVIPUsers();
+
+    } catch (error) {
+
+    }
+
+
+    this._loading = false;
+  }
+
+  onSelectionChange($event: any): void {
+    this._getDataEvent.emit();
+  }
+
+
+  ngOnDestroy(): void {
+    this._destroy$.next();
+    this._destroy$.complete();
+  }
+  //#endregion
 
 }
