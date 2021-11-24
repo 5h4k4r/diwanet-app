7fa4d57f45449eabaed8be93962317022cfdce88
5h4k4r <shakar.halabjai@yahoo.com>
2021-11-24 14:09:48 +0300

    Updated app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    document.documentElement.style.setProperty('--ion-font-family', 'Opensans regular');
+  }
 }
