07c734a0768ca2aaaca331f7b9927bbfe905ac54
5h4k4r <shakar.halabjai@yahoo.com>
2021-11-29 00:54:59 +0300

    Updated posts

--- src/app/pages/tabs/posts/posts.page.ts
+++ src/app/pages/tabs/posts/posts.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostsService } from 'src/app/backend/services/posts.service';
 
 @Component({
   selector: 'app-posts',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts.page.scss'],
 })
 export class PostsPage implements OnInit {
+  //#region Fields
 
-  constructor() { }
+
+  //#endregion
+
+
+  //#region Properties
+
+
+  //#endregion
+
+
+  //#region Constructor
+
+  constructor(
+    private postsService: PostsService
+  ) { }
+
+
+  //#endregion
+
+
+
+  //#region Functions
+
 
   ngOnInit() {
+    this.getCustomerPosts();
   }
 
+  async getCustomerPosts(): Promise<void> {
+    const posts = await this.postsService.getCustomerPosts();
+    console.log(posts);
+  }
+  //#endregion
+
 }
+
