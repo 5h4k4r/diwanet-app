e73c8193b36fa1acf26d180533b9e850179c78e8
5h4k4r <shakar.halabjai@yahoo.com>
2021-12-01 03:11:50 +0300

    Updated categories tab

--- src/app/pages/tabs/categories/categories.module.ts
+++ src/app/pages/tabs/categories/categories.module.ts
@@ -7,14 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { CategoriesPageRoutingModule } from './categories-routing.module';
 
 import { CategoriesPage } from './categories.page';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    CategoriesPageRoutingModule
+    CategoriesPageRoutingModule,
+    SharedModule,
   ],
   declarations: [CategoriesPage]
 })
-export class CategoriesPageModule {}
+export class CategoriesPageModule { }
--- src/app/pages/tabs/categories/categories.page.ts
+++ src/app/pages/tabs/categories/categories.page.ts
@@ -1,4 +1,9 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/backend/models/category.model';
+import { ServiceMan } from 'src/app/backend/models/serviceman.model';
+import { CategoriesService } from 'src/app/backend/services/categories.service';
+import { UsersService } from 'src/app/backend/services/users.service';
 
 @Component({
   selector: 'app-categories',
@@ -6,10 +11,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.page.scss'],
 })
 export class CategoriesPage implements OnInit {
+  //#region Fields
+  private _selectedCategory: Category;
+  private _loading = false;
+  private _categories: Category[];
+  private _users: ServiceMan[];
+  //#endregion
 
-  constructor() { }
+
+  //#region Properties
+  get selectedCategory(): Category { return this._selectedCategory; }
+  get loading(): boolean { return this._loading; }
+  get categories(): Category[] { return this._categories; }
+  get users(): ServiceMan[] { return this._users; }
+  //#endregion
+
+
+  //#region Constructor
+
+  constructor(
+    private categoriesService: CategoriesService,
+    private usersService: UsersService,
+  ) { }
+
+
+  //#endregion
+
+
+  //#region Functions
+
 
   ngOnInit() {
+    this.getCategories();
   }
+
+  async getCategories(): Promise<void> {
+    this._loading = true;
+
+    try {
+      this._categories = await this.categoriesService.getCategories();
+    } catch (error) {
+
+    }
+    this._loading = false;
+  }
+
+  async selectCategory(cat: Category): Promise<void> {
+    this._loading = true;
+    this._selectedCategory = cat;
+    try {
+      this._users = await this.usersService.listServiceMen({ service_cat_id: this.selectedCategory.id });
+    } catch (error) {
+
+    }
+    this._loading = false;
+  }
+
+  clearCategory(): void {
+    this._selectedCategory = null;
+  }
+  //#endregion
 
 }
