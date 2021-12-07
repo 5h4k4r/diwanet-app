968da57e987f4aedfdde3f1481a8eefeac93c7dd
5h4k4r <shakar.halabjai@yahoo.com>
2021-12-07 13:28:52 +0300

    Updated news service

--- src/app/backend/services/news.service.ts
+++ src/app/backend/services/news.service.ts
@@ -1,8 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { News } from '../models/news.model';
-
+class ImageSnippet {
+  constructor(public src: string, public file: File) { }
+}
 @Injectable({
   providedIn: 'root'
 })
@@ -18,9 +20,19 @@ export class NewsService {
   getNews(): Promise<News[]> {
     return this.http.get<News[]>(this.baseUrl).toPromise();
   }
+
   getNewsById(params: {
     id: number;
   }): Promise<News> {
     return this.http.get<News>(`${this.baseUrl}/${params.id}`).pipe(map(newsList => newsList[0])).toPromise();
+  }
+
+  addNews(params: {
+    title: string;
+    detail: string;
+    images: string[];
+  }): Promise<News> {
+
+    return this.http.post<News>(this.baseUrl, params).toPromise();
   }
 }
